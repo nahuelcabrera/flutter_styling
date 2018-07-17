4c8dac1466e943e8e29a7c4fc02997c5e94f884c
nahuelcabrera <nahuelcabrerawork@gmail.com>
2018-07-17 19:49:51 -0300

    spannable text

--- lib/main.dart
+++ lib/main.dart
@@ -22,23 +22,38 @@ class MyApp extends StatelessWidget {
                     Color(0xffef5350),
                     Color(0x00ef5350),
                   ]),
-                  borderRadius: BorderRadius.circular(10.0),
-                  border: Border.all(
-                    color: Colors.orange
-                  )
+                  //borderRadius: BorderRadius.circular(1000.0),
+                  //border: Border.all(color: Colors.orange),
+                  boxShadow: <BoxShadow>[
+                    BoxShadow(color: Color(0xcc000000), offset: Offset(20.0, 30.0), blurRadius: 4.0),
+                    BoxShadow(color: Color(0x80000000), offset: Offset(-20.0, 50.0), blurRadius: 2.0),
+                  ],
+                  shape: BoxShape.rectangle,
                 ),
-                child: Text(
-                  'Styling stuff',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.black54,
-                    fontSize: 42.0,
-                    fontWeight: FontWeight.w600,
-                    fontFamily: 'Roboto',
-                    fontStyle: FontStyle.normal,
-                  ),
-                ),
-              ),)
+                child: RichText(
+                    text: TextSpan(
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 38.0,
+                      ),
+                      children: <TextSpan>[
+                        TextSpan(
+                          text: 'Styling ',
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontSize: 28.0,
+                          ),),
+                        TextSpan(
+                          text: 'Stuff papa',
+                          style: TextStyle(
+                            color: Colors.white.withOpacity(0.3),
+                            fontWeight: FontWeight.w700
+                          ),),
+                      ]
+                    )
+                )
+              ),
+            ),
           ),
         ),
       ),
